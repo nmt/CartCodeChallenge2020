599d8d3fe08bd898f3de43f5838cb8f84dce0555
Nathalia Tan <nathalia.tan@deloitte.com>
2021-01-05 00:05:08 +1100

    [FEAT] Added snapshot test for App

--- src/App-UI.test.tsx
+++ src/App-UI.test.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer';
+
+test('renders App snapshot', () => {
+  const tree = renderer
+    .create(<App />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
 test('renders profile selector', () => {
   render(<App />);
