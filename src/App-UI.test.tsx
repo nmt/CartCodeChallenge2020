14a0059418c24ad10a21e253880f4d0a593b36bf
Nathalia Tan <nathalia.tan@deloitte.com>
2021-01-05 11:36:00 +1100

    [FEAT] Tests for QuantityPicker functionality

--- src/App-UI.test.tsx
+++ src/App-UI.test.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
+import QuantityPicker from './components/QuantityPicker';
 import renderer from 'react-test-renderer';
+
+test('Quantity Picker component increases quantity', () => {
+  const itemProp = {"name":"classic","quantity":0,"price":26999,"subtotal":0};
+  const qp = shallow(<QuantityPicker id="testQp" type="classic" quantity={0} item={itemProp} />);
+  expect(qp.text()).toEqual('0+-');
+  qp.find('.increaseButton').simulate('click');
+  expect(qp.text()).toEqual('1+-');
+});
+
+test('Quantity Picker component decreases quantity', () => {
+  const itemProp = {"name":"classic","quantity":0,"price":26999,"subtotal":0};
+  const qp = shallow(<QuantityPicker id="testQp" type="classic" quantity={0} item={itemProp} />);
+  expect(qp.text()).toEqual('0+-');
+  qp.find('.increaseButton').simulate('click');
+  expect(qp.text()).toEqual('1+-');
+  qp.find('.decreaseButton').simulate('click');
+  expect(qp.text()).toEqual('0+-');
+});
+
+test('Quantity Picker component does not decrease quantity past 0', () => {
+  const itemProp = {"name":"classic","quantity":0,"price":26999,"subtotal":0};
+  const qp = shallow(<QuantityPicker id="testQp" type="classic" quantity={0} item={itemProp} />);
+  expect(qp.text()).toEqual('0+-');
+  qp.find('.increaseButton').simulate('click');
+  expect(qp.text()).toEqual('1+-');
+  qp.find('.decreaseButton').simulate('click');
+  expect(qp.text()).toEqual('0+-');
+  qp.find('.decreaseButton').simulate('click');
+  expect(qp.text()).toEqual('0+-');
+});
 
 test('renders App snapshot', () => {
   const tree = renderer
