31fcf76fb1834a79beb66674199e17681b90cfbb
Nathalia Tan <nathalia.tan@deloitte.com>
2021-01-05 17:56:39 +1100

    [RF] QuantityPicker no longer has its own state to prevent issues when changing profiles

--- src/App-Components.test.tsx
+++ src/App-Components.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import App from './App';
 import Profile from './components/Profile';
-import QuantityPicker from './components/QuantityPicker';
 
 test('Profile component should have 4 options', () => {
   const profile = shallow(<Profile />);
@@ -16,31 +16,28 @@ test('Profile component should update its value when changed', () => {
 });
 
 test('Quantity Picker component increases quantity', () => {
-  const itemProp = {"name":"classic","quantity":0,"price":26999,"subtotal":0};
-  const qp = shallow(<QuantityPicker id="testQp" type="classic" quantity={0} item={itemProp} />);
-  expect(qp.text()).toEqual('0+-');
-  qp.find('.increaseButton').simulate('click');
-  expect(qp.text()).toEqual('1+-');
+  const app = mount(<App />);
+  expect(app.find('#classicAdPicker .quantityPickerText').text()).toEqual('0');
+  app.find('#classicAdPicker .increaseButton').simulate('click');
+  expect(app.find('#classicAdPicker .quantityPickerText').text()).toEqual('1');
 });
 
 test('Quantity Picker component decreases quantity', () => {
-  const itemProp = {"name":"classic","quantity":0,"price":26999,"subtotal":0};
-  const qp = shallow(<QuantityPicker id="testQp" type="classic" quantity={0} item={itemProp} />);
-  expect(qp.text()).toEqual('0+-');
-  qp.find('.increaseButton').simulate('click');
-  expect(qp.text()).toEqual('1+-');
-  qp.find('.decreaseButton').simulate('click');
-  expect(qp.text()).toEqual('0+-');
+  const app = mount(<App />);
+  expect(app.find('#classicAdPicker .quantityPickerText').text()).toEqual('0');
+  app.find('#classicAdPicker .increaseButton').simulate('click');
+  expect(app.find('#classicAdPicker .quantityPickerText').text()).toEqual('1');
+  app.find('#classicAdPicker .decreaseButton').simulate('click');
+  expect(app.find('#classicAdPicker .quantityPickerText').text()).toEqual('0');
 });
 
 test('Quantity Picker component does not decrease quantity past 0', () => {
-  const itemProp = {"name":"classic","quantity":0,"price":26999,"subtotal":0};
-  const qp = shallow(<QuantityPicker id="testQp" type="classic" quantity={0} item={itemProp} />);
-  expect(qp.text()).toEqual('0+-');
-  qp.find('.increaseButton').simulate('click');
-  expect(qp.text()).toEqual('1+-');
-  qp.find('.decreaseButton').simulate('click');
-  expect(qp.text()).toEqual('0+-');
-  qp.find('.decreaseButton').simulate('click');
-  expect(qp.text()).toEqual('0+-');
+  const app = mount(<App />);
+  expect(app.find('#classicAdPicker .quantityPickerText').text()).toEqual('0');
+  app.find('#classicAdPicker .increaseButton').simulate('click');
+  expect(app.find('#classicAdPicker .quantityPickerText').text()).toEqual('1');
+  app.find('#classicAdPicker .decreaseButton').simulate('click');
+  expect(app.find('#classicAdPicker .quantityPickerText').text()).toEqual('0');
+  app.find('#classicAdPicker .decreaseButton').simulate('click');
+  expect(app.find('#classicAdPicker .quantityPickerText').text()).toEqual('0');
 });
--- src/components/QuantityPicker.tsx
+++ src/components/QuantityPicker.tsx
@@ -8,51 +8,35 @@ interface QuantityPickerProps {
     onQuantityChange?: Function,
 }
 
-type QuantityPickerState = {
-    quantity: number,
-}
-
-class QuantityPicker extends React.Component<QuantityPickerProps, QuantityPickerState> {
+class QuantityPicker extends React.Component<QuantityPickerProps, {}> {
     constructor(props: any) {
         super(props);
 
         this.increaseQuantity = this.increaseQuantity.bind(this);
         this.decreaseQuantity = this.decreaseQuantity.bind(this);
-
-        this.state = {
-            quantity: this.props.quantity,
-        };
     }
 
     increaseQuantity() {
-        let newQuantity = this.state.quantity;
+        let newQuantity = this.props.quantity;
         newQuantity = newQuantity + 1;
         this.props.onQuantityChange?.(this.props.type, newQuantity);
-
-        this.setState({
-            quantity: newQuantity,
-        })
     }
 
     decreaseQuantity() {
-        let newQuantity = this.state.quantity;
+        let newQuantity = this.props.quantity;
         // Ensure quantity cannot go into negatives
         if (newQuantity > 0) {
             newQuantity = newQuantity - 1;
             this.props.onQuantityChange?.(this.props.type, newQuantity);
         }
-
-        this.setState({
-            quantity: newQuantity,
-        })
     }
 
     render() {
-        const { id } = this.props;
+        const { id, quantity } = this.props;
         return(
             <div id={id + 'Picker'} className="quantityPicker">
                 <div className="quantityPickerText">
-                    <p>{this.state.quantity}</p>
+                    <p>{quantity}</p>
                 </div>
                 <div className="quantityPickerButtonHolder">
                     <button className="increaseButton" onClick={this.increaseQuantity}>+</button>
