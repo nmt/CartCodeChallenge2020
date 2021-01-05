bc33ffa61938b3384566aeec8daca1692f3f5b88
Nathalia Tan <nathalia.tan@deloitte.com>
2021-01-05 11:35:22 +1100

    [FEAT] QuantityPicker now maintains quantity in its state

--- src/components/QuantityPicker.tsx
+++ src/components/QuantityPicker.tsx
@@ -5,40 +5,58 @@ interface QuantityPickerProps {
     type: string,
     quantity: number,
     item: Object,
-    onQuantityChange: Function,
+    onQuantityChange?: Function,
 }
 
-class QuantityPicker extends React.Component<QuantityPickerProps, {}> {
+type QuantityPickerState = {
+    quantity: number,
+}
+
+class QuantityPicker extends React.Component<QuantityPickerProps, QuantityPickerState> {
     constructor(props: any) {
         super(props);
 
         this.increaseQuantity = this.increaseQuantity.bind(this);
         this.decreaseQuantity = this.decreaseQuantity.bind(this);
+
+        this.state = {
+            quantity: this.props.quantity,
+        };
     }
 
     increaseQuantity() {
-        const newQuantity = this.props.quantity + 1;
-        this.props.onQuantityChange(this.props.type, newQuantity);
+        let newQuantity = this.state.quantity;
+        newQuantity = newQuantity + 1;
+        this.props.onQuantityChange?.(this.props.type, newQuantity);
+
+        this.setState({
+            quantity: newQuantity,
+        })
     }
 
     decreaseQuantity() {
+        let newQuantity = this.state.quantity;
         // Ensure quantity cannot go into negatives
-        if (this.props.quantity > 0) {
-            const newQuantity = this.props.quantity - 1;
-            this.props.onQuantityChange(this.props.type, newQuantity);
+        if (newQuantity > 0) {
+            newQuantity = newQuantity - 1;
+            this.props.onQuantityChange?.(this.props.type, newQuantity);
         }
+
+        this.setState({
+            quantity: newQuantity,
+        })
     }
 
     render() {
-        const { id, quantity } = this.props;
+        const { id } = this.props;
         return(
             <div id={id + 'Picker'} className="quantityPicker">
                 <div className="quantityPickerText">
-                    <p>{quantity}</p>
+                    <p>{this.state.quantity}</p>
                 </div>
                 <div className="quantityPickerButtonHolder">
-                    <button onClick={this.increaseQuantity}>+</button>
-                    <button onClick={this.decreaseQuantity}>-</button>
+                    <button className="increaseButton" onClick={this.increaseQuantity}>+</button>
+                    <button className="decreaseButton" onClick={this.decreaseQuantity}>-</button>
                 </div>
             </div>
         );
