e7906cd5771bf9ab73202d21b6d09f14ec488d9b
Nathalia Tan <nathalia.tan@deloitte.com>
2021-01-05 12:19:28 +1100

    [FEAT] Profile now maintains profile in its state

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 
 interface ProfileProps {
-  onProfileChange: Function;
+  onProfileChange?: Function;
 }
 
-class Profile extends React.Component<ProfileProps, {}> {
+type ProfileState = {
+  profile: string,
+}
+
+class Profile extends React.Component<ProfileProps, ProfileState> {
   constructor(props: any) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      profile: '',
+    };
   }
 
   handleChange(e: any) {
-    this.props.onProfileChange(e.target.value);
+    this.props.onProfileChange?.(e.target.value);
+
+    this.setState({
+      profile: e.target.value,
+    });
   }
   
   render() {
@@ -19,10 +31,10 @@ class Profile extends React.Component<ProfileProps, {}> {
       <div id="profileSelector" className="profileSelector">
         <label htmlFor="profile">Select a profile: </label>
         <select name="profile" onChange={this.handleChange}>
-          <option value="">Default</option>
-          <option value="secondBite">SecondBite</option>
-          <option value="axil">Axil Coffee Roasters</option>
-          <option value="myer">MYER</option>
+          <option ref="" value="">Default</option>
+          <option ref="secondBite" value="secondBite">SecondBite</option>
+          <option ref="axil" value="axil">Axil Coffee Roasters</option>
+          <option ref="myer" value="myer">MYER</option>
         </select>
       </div>
     );
